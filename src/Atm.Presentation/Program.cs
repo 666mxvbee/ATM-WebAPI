41e612f6663a7bc310f57eddefe4927857593a5d
666mxvbee <mitrasher.negativ@gmail.com>
2026-05-17 19:57:59 +0300

    fix(presentation): add swaggerUI

--- src/Atm.Presentation/Program.cs
+++ src/Atm.Presentation/Program.cs
@@ -11,7 +11,9 @@ builder.Services.AddControllers().AddJsonOptions(options =>
     options.JsonSerializerOptions.ReadCommentHandling = System.Text.Json.JsonCommentHandling.Skip;
     options.JsonSerializerOptions.AllowTrailingCommas = true;
 });
+
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure();
@@ -20,8 +22,13 @@ builder.Services.AddSingleton<ISystemPasswordProvider, SystemPasswordProvider>()
 
 WebApplication app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.UseHttpsRedirection();
-app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
